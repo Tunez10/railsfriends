d3c208845aaa514dc38554623676b4b53f83b8da
Peter Fortune <peterekene162@gmail.com>
2025-07-21 12:01:58 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.{html,erb}",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.{css,scss}",
+    "./app/frontend/**/*.{js,jsx,ts,tsx,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
